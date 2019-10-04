fbec0db7cfc365369ce3883189c0335349e04ac0
Roman Aleksanov <alexanov.roman@gmail.com>
2019-10-05 01:44:48 +0300

    Files uploading functionality was added

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,9 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Response, Post, UseInterceptors, UploadedFile, Param } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 @Controller('files')
 export class FilesController {
-  @Get()
-  public getFiels(): any {
-    return [1, 2, 3];
+  @Get(':fileName')
+  public getFile(@Param('fileName') fileName, @Response() res): any {
+    return res.sendFile(fileName, { root: 'uploads' });
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file', {
+    storage: diskStorage({
+      destination: './uploads',
+      filename: (_, file, cb) => {
+        const randomName = Array(32).fill(null).map(() => (Math.round(Math.random() * 16)).toString(16)).join('');
+
+        return cb(null, `${randomName}${extname(file.originalname)}`);
+      }
+    })
+  }))
+  public uploadFile(@UploadedFile() file): any {
+    return file;
   }
 }
